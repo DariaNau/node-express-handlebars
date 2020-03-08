92ec9c956e1b2dfde9a8f06e0a8d3da9023439c7
Daria <dariagnaumova@gmail.com>
2020-03-07 22:44:02 -0500

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Finnycat1206!",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "gt2020",
+    database: "burger_db"
+  });
+}
 
 // Make connection.
 connection.connect(err => {
